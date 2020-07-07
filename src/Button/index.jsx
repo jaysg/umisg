03f78b6e3583ada50ee8f6daac9198d792c08177
jj.shangguan <a8022476@yeah.net>
2020-07-07 16:41:42 +0800

    Button demo change

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import classnames from "classnames";
 
 import styles from "./index.module.less";
 import "./index.css";
 
-export default function(props) {
+const Component = (props) => {
+  const [count, setCount] = useState(0);
   const { color, children } = props;
   return (
     <button
       style={{ color }}
       className={classnames(styles.large, styles.bold, "backgroud")}
+      onClick={() => setCount(count + 1)}
     >
-      {children}
+      {children} click times:{count}
     </button>
   );
-}
+};
+
+export default Component;
